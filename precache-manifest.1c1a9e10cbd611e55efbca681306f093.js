0ff726d4be64890e435944d9ada5513520f8d062
issaoui-rami <ramiissaoui55@gmail.com>
2020-05-03 16:37:22 +0200

    Updates

---
+++ precache-manifest.1c1a9e10cbd611e55efbca681306f093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710dd117f55d0d10211e09e63bafb0e2",
+    "url": "/appcontacts/index.html"
+  },
+  {
+    "revision": "7ee891d8bd94e83cc767",
+    "url": "/appcontacts/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9c6fb5641422eaa67a8e",
+    "url": "/appcontacts/static/css/main.48bb660c.chunk.css"
+  },
+  {
+    "revision": "7ee891d8bd94e83cc767",
+    "url": "/appcontacts/static/js/2.d6098131.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/appcontacts/static/js/2.d6098131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6fb5641422eaa67a8e",
+    "url": "/appcontacts/static/js/main.566512ff.chunk.js"
+  },
+  {
+    "revision": "e6952b6ec740eb3f76db",
+    "url": "/appcontacts/static/js/runtime-main.a0aeee65.js"
+  }
+]);
